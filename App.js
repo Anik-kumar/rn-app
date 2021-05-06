f9aae0fb41fce2d210ed75d13b55c98a53bc7ec8
Anik-kumar <Anik-kumar@users.noreply.github.com>
2021-05-06 16:35:30 +0600

    first commit :)

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 30, border: 1}}>
+      <View>
+        <TextInput placeholder="Course Goal" />
+        <Button title="Add" />
+      </View>
+      <View></View>
     </View>
   );
 }
